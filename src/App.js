05c92af4eff6d806d5bed8ca3ecefc6b77dbda2f
mark19242 <mark19242@gmail.com>
2024-02-28 10:03:37 -0800

    Game board is ready

--- src/App.js
+++ src/App.js
@@ -15,12 +15,18 @@ const App = () => {
     "?"
   ])
 
+  const handleGamePlay = (currentSquare) => {
+      alert(currentSquare)
+  }
+
   return (
     <>
       <h1>Treasure Hunt Game</h1>
       <div className="board">
       {board.map((square, index)=>{
-        return <Square square={square} />
+        return <Square square={square} 
+        index={index} 
+        handleGamePlay={handleGamePlay} />
       })}
       </div>
     </>
